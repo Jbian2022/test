7f2d3968153047ec9464ff30c73594af8a014172
lijidong <15569254080@163.com>
2022-11-14 16:33:04 +0800

    全局注册vant插件

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import i18n from './lang/i18n'
 
 // #ifndef VUE3
 import Vue from 'vue'
+import Vant from 'vant';
+import 'vant/lib/index.css'
+Vue.use(vant)
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
@@ -16,10 +19,13 @@ app.$mount()
 
 // #ifdef VUE3
 import {createSSRApp} from 'vue'
+import Vant from 'vant';
+import 'vant/lib/index.css'
 
 export function createApp() {
 	const app = createSSRApp(App)
 	app.use(i18n)
+	app.use(Vant)
 	return {app}
 }
 // #endif
