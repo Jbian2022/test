7f9b77c971f23804af4eba5c3a5954d5cc4f0205
lijidong <15569254080@163.com>
2022-11-16 17:41:13 +0800

    页面开发

--- pages/addMyMebers/addMyMebers.vue
+++ pages/addMyMebers/addMyMebers.vue
@@ -1,6 +1,43 @@
 <template>
 	<view class="content_style">
 		 <BgTheamCompontent :theamType="'currency'"></BgTheamCompontent>
+<!-- 		 <van-nav-bar
+		   title=""
+		   left-text="添加学员"
+		   left-arrow
+		   @click-left="onClickLeft"
+		 /> -->
+		 <view class="contetnt_form_style">
+			 <van-form @submit="onSubmit">
+			   <van-cell-group inset>
+			     <van-field
+			       v-model="username"
+			       name="用户名"
+			       label="用户名"
+			       placeholder="用户名"
+				  
+			       :rules="[{ required: true, message: '请填写用户名' }]"
+			     />
+			   </van-cell-group>
+			   <van-cell-group inset>
+			     <van-field
+			       v-model="username"
+			       name="用户名"
+			       label="用户名"
+			       placeholder="用户名"
+			   				  
+			       :rules="[{ required: true, message: '请填写用户名' }]"
+			     />
+			    
+			   </van-cell-group>
+			   
+			   <div style="margin: 16px;">
+			     <van-button round block type="primary" native-type="submit">
+			       提交
+			     </van-button>
+			   </div>
+			 </van-form>
+		 </view>
 		
 	</view>
 </template>
@@ -19,6 +56,37 @@
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.van-hairline--bottom:after {
+  border-bottom-width: 0;
+   border-bottom-color: #212328;
+} 
+
+	::v-deep.van-nav-bar__content {
+		background: #212328;
+		border: none;
+		height: 88upx;
+		box-sizing: border-box;
+
+		
+	}
+	::v-deep.van-nav-bar .van-icon {
+		color: #fff;
+	}
+	::v-deep.van-nav-bar .van-nav-bar__text {
+		color: #fff !important;
+	}
+	.content_style {
+		width: 100vw;
+		height: 100vh;
+		overflow: hidden;
+		position: relative;
+		.contetnt_form_style {
+			width: 100%;
+			margin-top: 30upx;
+			
+			
+		}
+	}
 
 </style>
