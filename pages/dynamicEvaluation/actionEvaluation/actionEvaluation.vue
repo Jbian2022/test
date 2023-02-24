46e32799a32bae9cea422aa0d98c26fa02d8d3c0
lijidong <15569254080@163.com>
2023-02-24 16:30:34 +0800

    优化代码

--- pages/dynamicEvaluation/actionEvaluation/actionEvaluation.vue
+++ pages/dynamicEvaluation/actionEvaluation/actionEvaluation.vue
@@ -44,7 +44,7 @@
             class="clickActionContent"
           >
             <view class="clickActionBody">
-              <video
+              <!-- <video
                 :src="FrontVideoUrl"
                 wid
                 autoplay
@@ -52,7 +52,27 @@
                 :controls="false"
                 muted
                 v-if="show"
-              ></video>
+              ></video> -->
+              <view v-if="!checkFile(FrontVideoUrl)">
+                <video
+                  :src="FrontVideoUrl"
+                  wid
+                  autoplay
+                  :custom-cache="false"
+                  muted
+                  v-if="show"
+                ></video>
+              </view>
+
+              <view
+                v-else
+                style="margin: 0 auto; padding-left: 40upx; padding-top: 40upx"
+              >
+                <image
+                  style="border-radius: 32upx; height: 400upx"
+                  :src="FrontVideoUrl"
+                ></image>
+              </view>
               <view class="clickActionText">
                 <view class="Actionname">标准动作：</view>
                 <view>
@@ -107,16 +127,27 @@
             class="clickActionContent"
           >
             <view class="clickActionBody">
-              <video
-                :src="SideVideoUrl"
-                wid
-                autoplay
-                loop
-                :controls="false"
-                :custom-cache="false"
-                muted
-                v-if="show2"
-              ></video>
+              <view v-if="!checkFile(SideVideoUrl)">
+                <video
+                  :src="SideVideoUrl"
+                  wid
+                  autoplay
+                  loop
+                  :controls="false"
+                  :custom-cache="false"
+                  muted
+                  v-if="show2"
+                ></video>
+              </view>
+              <view
+                v-else
+                style="margin: 0 auto; padding-left: 40upx; padding-top: 40upx"
+              >
+                <image
+                  style="border-radius: 32upx; height: 400upx"
+                  :src="SideVideoUrl"
+                ></image>
+              </view>
               <view class="clickActionText">
                 <view class="Actionname">标准动作：</view>
                 <view>
@@ -175,16 +206,27 @@
             class="clickActionContent"
           >
             <view class="clickActionBody">
-              <video
-                :src="SideVideoUrl"
-                wid
-                autoplay
-                loop
-                :controls="false"
-                :custom-cache="false"
-                muted
-                v-if="show2"
-              ></video>
+              <view v-if="!checkFile(SideVideoUrl)">
+                <video
+                  :src="SideVideoUrl"
+                  wid
+                  autoplay
+                  loop
+                  :controls="false"
+                  :custom-cache="false"
+                  muted
+                  v-if="show2"
+                ></video>
+              </view>
+              <view
+                v-else
+                style="margin: 0 auto; padding-left: 40upx; padding-top: 40upx"
+              >
+                <image
+                  style="border-radius: 32upx; height: 400upx"
+                  :src="SideVideoUrl"
+                ></image>
+              </view>
               <view class="clickActionText">
                 <view class="Actionname">标准动作：</view>
                 <view>
@@ -447,6 +489,20 @@ export default {
         this.changeValue3 = true
         this.num = index
       }
+    },
+    checkFile(url) {
+      console.log('url==' + url)
+      var index = url.indexOf('.') //（考虑严谨用lastIndexOf(".")得到）得到"."在第几位
+      var urlSuffix = url.substring(index) //截断"."之前的，得到后缀
+      if (!/(.*)\.(mp4|rmvb|avi|ts)$/.test(urlSuffix)) {
+        //根据后缀，判断是否符合视频格式
+        return true
+      }
+      if (!/(.*)\.(gif|jpg|jpeg|png|GIF|JPG|PNG)$/.test(urlSuffix)) {
+        //根据后缀，判断是否符合图片格式
+        return false
+      }
+      return true
     },
     getActionInfo() {
       if (this.type !== '') {
